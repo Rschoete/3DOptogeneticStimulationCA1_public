3e2cce1a2c4639ceab963e4b30114c80eda10de1
Ruben Schoeters <Ruben.Schoeters@ugent.be>
2022-09-29 11:21:15 +0200

    Update createInput_set.py

--- createInput_set.py
+++ createInput_set.py
@@ -20,34 +20,33 @@ if __name__ == '__main__':
 
     inputsoptall = []
 
-    Celltemplates = Cells.NeuronTemplates[:4]
+    Celltemplates = Cells.NeuronTemplates[:1]
     #opsinlocs_pyrs = #['soma','axon','all','alldend','apic','basal','apicaltrunk','apicaltrunk_ext','apicaltuft','obliques','apicalnotuft']
-    opsinlocs_pyrs = ['all','soma','apic']#,'alldend','apic','basal','apicalnotuft']
-    opsinlocs_interss = ['all','soma']#,'alldend']
+    opsinlocs_pyrs = ['all','soma','apic','basal']#,'alldend','apic','basal','apicalnotuft']
+    opsinlocs_interss = ['all','soma','alldend']
     Gmaxs = [None]+list(np.logspace(-1,1,5))
     distributions = [f'distribution = lambda x: {y}' for y in list(np.logspace(1,3,5))]
-    rots = list(2/12*np.pi*np.arange(4))
-    xposs = list(np.arange(0,2000,200))
-    zposs = list(np.arange(0,5000,500))
     pds = [0.01,0.1,1,10,100]
     dcs = [1/100,1/10,1/2]
 
-    for nrp  in [1,2]:
-        for cell in Celltemplates:
-            sublist = []
-            if 'pyr' in cell.lower():
-                opsinlocs = opsinlocs_pyrs
-            else:
-                opsinlocs = opsinlocs_interss
-            for opsinloc in opsinlocs:
+
+    for cell in Celltemplates:
+        if 'pc' in cell.lower():
+            opsinlocs = opsinlocs_pyrs
+        else:
+            opsinlocs = opsinlocs_interss
+        for opsinloc in opsinlocs:
+            for nrp  in [1,2]:
+                dcs_intm = dcs if nrp>1 else [1]
                 iter = -1
+                sublist = []
                 for Gmax in Gmaxs:
                     if Gmax is None:
                         distrs = distributions
                     else:
                         distrs = [f'distribution = lambda x: {1}']
                     for distr in distrs:
-                        for dc in dcs:
+                        for dc in dcs_intm:
 
                             iter+=1
 
@@ -55,31 +54,30 @@ if __name__ == '__main__':
                             inputsoptall.append(filenameopt)
                             sublist.append(filenameopt)
 
-                            input = stp.simParams({'test_flag':False,'save_flag': True, 'plot_flag': False})
+                            input = stp.simParams({'test_flag':False,'save_data_flag':False,'save_input_flag':False,'save_flag': True, 'plot_flag': False})
 
-                            input.resultsFolder = '/SDC'
-                            input.subfolderSuffix = f'{cell}_{opsinloc}_np{nrp}_{iter}'
+                            input.resultsFolder = '/SDC/'+f'{cell}_{opsinloc}_np{nrp}_{iter}'
+                            input.subfolderSuffix = ''
+
+                            input.duration = 100
+                            input.v0 = -70
 
                             input.stimopt.stim_type = ['Optogxstim']
-                            input.simulationType = ['normal','SD_Optogenx']
+                            input.simulationType = ['SD_Optogenx']
 
                             input.cellsopt.neurontemplate = cell
 
                             input.cellsopt.opsin_options.opsinlocations = opsinloc
                             input.cellsopt.opsin_options.Gmax_total = Gmax #uS
                             input.cellsopt.opsin_options.distribution = distr
-                            input.cellsopt.init_options.theta = 0
-
-                            input.duration = 100+10 * 10/dc+100
-                            input.v0 = -70
-
+                            input.cellsopt.init_options.theta = -np.pi/2 # allign axo-somato-dendritic axis with z-axis
 
                             input.stimopt.Ostimparams.filepath = 'Inputs/LightIntensityProfile/Ugent470nIrr_np1e7_res5emin3_gf1_cyl_5x10.txt'
                             input.stimopt.Ostimparams.amp = 1/3*10**5
                             input.stimopt.Ostimparams.delay = 100
                             input.stimopt.Ostimparams.pulseType = 'pulseTrain'
                             input.stimopt.Ostimparams.dur = 10 * 10/dc
-                            input.stimopt.Ostimparams.options = {'prf':dc/10,'dc':dc, 'psi': np.pi/2, 'xT': [0,0,0]}
+                            input.stimopt.Ostimparams.options = {'prf':dc/10,'dc':dc, 'xT': [0,0,0]}
 
                             input.analysesopt.SDOptogenx.options['simdur']=500
                             input.analysesopt.SDOptogenx.options['delay']=100
@@ -92,18 +90,18 @@ if __name__ == '__main__':
                             input.analysesopt.SDOptogenx.nr_pulseOI = nrp
 
 
-                            input.analysesopt.succesRatioOptions['window'] = 100
+                            input.analysesopt.recordSuccesRatio = False
                             input.analysesopt.sec_plot_flag = False
-                            input.analysesopt.recordTraces = {}
+                            input.analysesopt.recordTraces = Dict()
                             input.analysesopt.save_traces = False
 
                             with open(filenameopt,'w') as file:
                                 json.dump(input.todict(False), file, indent = 4, cls=MyEncoder)
-            
-            with open(folder+'sublist'+cell+str(nrp)+'.csv','w') as file:
-                file.write('inputfilename\n')
-                for  item in sublist:
-                    file.write("%s\n" % item)
+
+                with open(folder+'sublist'+cell+opsinloc+str(nrp)+'.csv','w') as file:
+                    file.write('inputfilename\n')
+                    for  item in sublist:
+                        file.write("%s\n" % item)
 
 
     with open(runlist_filenameoptall+'.txt','w') as file:
